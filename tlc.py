1d484a71bc668072df29e4b69622f643881fcc97
SepehrRasouli <sepehrrs06@gmail.com>
2022-07-15 17:35:42 +0430

    added handle_dir function and changed tlc.py so it would detect directories

--- tlc.py
+++ tlc.py
@@ -4,11 +4,15 @@ from Player.info import show_track_info
 from optparse import OptionParser
 from rich.console import Console
 from rich import print
-
 parser = OptionParser()
 (options, args) = parser.parse_args()
 
 console = Console()
+
+def handle_dirs(path:str):
+    tracks = os.listdir(path)
+    sorted(tracks)
+    return tracks
 
 def get_status_data():
     current_time = player.get_current_playtime()
@@ -30,6 +34,13 @@ def main():
 
 if __name__ == "__main__":
     # create player obj and show media info
+    if os.path.isdir(args[0]):
+        tracks = handle_dirs(args[0])
+        additional = f"[deep_pink4]{args[0]}[/deep_pink4]"
+        for track in tracks:
+            player = Player(args[0],additional)
+            show_track_info(player.media_load_info, player.tag)
+            player.play()
     player = Player(args[0])
     show_track_info(player.media_load_info, player.tag)
     player.play()
