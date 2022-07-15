6dc088872b9872bf87ba8bdd2f84b21533c50062
torhamdev <torhamdev@gmail.com>
2022-07-15 15:58:41 +0430

    Add frist tlc version :)

---
+++ tlc.py
@@ -0,0 +1,40 @@
+from time import sleep
+from Player.core import Player
+from Player.info import show_track_info
+from optparse import OptionParser
+from rich.console import Console
+from rich import print
+
+parser = OptionParser()
+(options, args) = parser.parse_args()
+
+console = Console()
+
+# create player obj and show media info
+player = Player(args[0])
+show_track_info(player.media_load_info, player.tag)
+player.play()
+
+
+def get_status_data():
+
+    current_time = player.get_current_playtime()
+
+    if player.is_played:
+        symbol_play = u"\u25b6"
+    else:
+        symbol_play = u"\u23f8"
+
+    return f"[bold green]{current_time} <=> {player.get_total_media_time()} " + symbol_play
+
+
+def main():
+    try:
+        with console.status(get_status_data()) as status:
+            while player.is_played:
+                sleep(0.6)
+                status.update(get_status_data())
+    except KeyboardInterrupt:
+        print('Bay :vulcan_salute:')
+
+main()
