0011acab88917dda090725fb10a0c4624bc38f62
SepehrRasouli <sepehrrs06@gmail.com>
2022-07-15 18:48:43 +0430

    Removed addtional info arg and added it to show track info

--- tlc.py
+++ tlc.py
@@ -36,11 +36,12 @@ if __name__ == "__main__":
     # create player obj and show media info
     if os.path.isdir(args[0]):
         tracks = handle_dirs(args[0])
-        additional = f"[deep_pink4]{args[0]}[/deep_pink4]"
+        additional_info = f"[deep_pink4]{args[0]}[/deep_pink4]"
         for track in tracks:
-            player = Player(args[0],additional)
-            show_track_info(player.media_load_info, player.tag)
+            player = Player(args[0])
+            show_track_info(player.media_load_info, player.tag,additional_info)
             player.play()
+            main()
     player = Player(args[0])
     show_track_info(player.media_load_info, player.tag)
     player.play()
