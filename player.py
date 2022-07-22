bc7de7c8112e907f3095db0e1d1a250c4afa70b9
SepehrRasouli <sepehrrs06@gmail.com>
2022-07-22 13:24:14 +0430

    Changed all variable names to camelCase and added get total media time

--- player.py
+++ player.py
@@ -10,37 +10,50 @@ class Player():
     Player object to control music.
     """
     def __init__(self,media_path) -> None:
-        self.media_path = media_path
-        self.media_player = vlc.MediaPlayer()
-        self.media = vlc.Media(self.media_path)
-        self.media_player.set_media(self.media)
+        self.mediaPath = mediaPath
+        self.mediaPlayer = vlc.MediaPlayer()
+        self.media = vlc.Media(self.mediaPath)
+        self.mediaPlayer.set_media(self.media)
         self.tag = id3.Tag()
-        self.tag.parse(self.media_path)
-        self.media_load_info = load(self.media_path)
-        self.is_played,self.is_paused,self.is_stopped = False,False,False
+        self.tag.parse(self.mediaPath)
+        self.mediaLoadInfo = load(self.mediaPath)
+        self.isPlayed,self.isPaused,self.isStopped = False,False,False
 
     def start(self) -> None:
         """
         Plays Music.
         """
         self.media_player.play()
-        self.is_played,self.is_paused,self.is_stopped = True,False,False
+        self.isPlayed,self.isPaused,self.isStopped = True,False,False
 
     def pause(self) -> None:
         """
         Pauses Music
         """
         self.media_player.pause()
-        self.is_played,self.is_paused,self.is_stopped = False,True,False
+        self.isPlayed,self.isPaused,self.isStopped = False,True,False
 
     def stop(self) -> None:
         """
         Stops Music.
         """
         self.media_player.stop()
-        self.is_played,self.is_paused,self.is_stopped = False,False,True
+        self.isPlayed,self.isPaused,self.isStopped = False,False,True
 
-    def getCurrentPlaytime(self):
+    def getCurrentPlaytime(self) -> str:
         millisecondsPlaytime = self.media_player.get_time()
         currentTime = str(datetime.timedelta(milliseconds=milliseconds_playtime)).split('.')[0]  # noqa
         return currentTime
+
+    def get_total_media_time(self) -> str:
+        """
+        Gets total media time
+
+        Return : Total time
+        """
+
+        try:
+            timeSecs = self.mediaLoadInfo.info.timeSecs
+            return str(datetime.timedelta(seconds=timeSecs)).split('.')[0]
+        except AttributeError:
+            return "00:00:00"
