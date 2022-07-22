b2f137a6849628f2df807ebab334a48d85bc0061
SepehrRasouli <sepehrrs06@gmail.com>
2022-07-22 13:57:35 +0430

    Finished playfordirs and playforpaths

--- tlc.py
+++ tlc.py
@@ -33,9 +33,9 @@ def handleDirs(path: str) -> list:
     """
     Returns music tracks from a target dir
 
-    Params : `path` : dir path
+    params : `path` : dir path
 
-    Retruns : `music tracks in target dir`
+    retrun : `music tracks in target dir`
     """
 
     tracks = [os.path.join(path, i) for i in os.listdir(path)]
@@ -43,4 +43,64 @@ def handleDirs(path: str) -> list:
     tracks = [i for i in filter(lambda x: x if os.path.isfile(x) else None, tracks)]  # noqa
     return tracks
 
+def getStatusData() -> str:
+    """
+    Returns play data status
 
+    return : `play data status string`
+    """
+
+    current_time = player.get_current_playtime()
+    if player.is_played:
+        symbol_play = u"\u25b6"
+    symbol_play = u"\u23f8"
+
+    return (
+        f"[bold green]{current_time} <=> {player.get_total_media_time()} "
+        + f"{current_track}/{all_tacks_sum} "
+        + symbol_play)
+
+def playForFiles(path:str):
+    """ 
+    Plays tracks for music paths
+    params : `path` : music path
+    """
+    player = Player(path)
+    show_track_info(player.media_load_info, player.tag, path_file)
+    main()
+
+def playForDirs(path:str):
+    """
+    Plays tracks for dirs
+
+    params : `path` : directory
+    """
+    tracks = handle_dirs(path)
+    allTracksSum = len(tracks)
+    for track in tracks:
+        console.clear()
+        player = Player(track)
+        show_track_info(
+            player.media_load_info,
+            player.tag,
+            path_file,
+        )
+        main()
+        current_track += 1
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    (option, args) = parser.parse_args()
+    console = Console()
+    console.clear()
+    if len(args) == 0:
+        print(
+            "[bold red]You must specify a path to file or dir like:[/bold red][blue]"
+            " tlc.py ~/Music/something.mp3 "
+            "or tlc.py ~/Music/ [/blue]"
+        )
+        quit()
+    path_file = path_file = args[0]
+    if os.path.isdir(args[0]):
+        playForDirs(args[0])
+    playforFiles(args[0])
